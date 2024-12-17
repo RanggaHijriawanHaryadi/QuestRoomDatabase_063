e4bd5c537521b944486de068a528526f20595d47
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-17 12:58:41 +0700

    memanggil fungsi getallmhs dengan menampilkan mahasiswa dalam list

--- app/src/main/java/com/example/pertemuan10/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10/repository/RepositoryMhs.kt
@@ -2,7 +2,12 @@ package com.example.pertemuan10.repository
 
 
 import com.example.pertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
+    //insertmhs
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    //getallmhs
+    fun getAllMhs(): Flow<List<Mahasiswa>>
 }
