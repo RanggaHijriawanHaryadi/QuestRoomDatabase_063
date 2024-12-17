9a96e337a2b64db99f35da47963eb50b7d93da69
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-17 14:06:33 +0700

    menambah private nim pada DestinasiDetail.NIM

--- app/src/main/java/com/example/pertemuan10/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,11 +1,19 @@
 package com.example.pertemuan10.ui.viewmodel
 
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.pertemuan10.data.entity.Mahasiswa
+import com.example.pertemuan10.repository.RepositoryMhs
+import com.example.pertemuan10.ui.navigation.DestinasiDetail
 
 class DetailMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+    ): ViewModel() {
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
 
-    )
+}
 /*
 Data class untuk menampung data yang akan ditampilkan diUI
 * */
