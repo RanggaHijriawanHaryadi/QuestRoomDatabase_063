33a38b7e3b3212c3b16a3bae6d79c289fe262893
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-17 13:37:02 +0700

    membuat repositoryMhs ke viewmodel

--- app/src/main/java/com/example/pertemuan10/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,11 +1,16 @@
 package com.example.pertemuan10.ui.viewmodel
 
 
+import androidx.lifecycle.ViewModel
 import com.example.pertemuan10.data.entity.Mahasiswa
+import com.example.pertemuan10.repository.RepositoryMhs
 
 
 class HomeMhsViewModel(
-)
+    private val repositoryMhs: RepositoryMhs
+): ViewModel() {
+
+}
 
 data class HomeUiState(
     val listMhs: List<Mahasiswa> = listOf(),
