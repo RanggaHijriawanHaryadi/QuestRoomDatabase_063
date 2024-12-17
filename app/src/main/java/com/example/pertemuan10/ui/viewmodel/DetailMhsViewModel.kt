cd0bf49d48cacd5727bcdfbd4b6b272d51e70678
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-17 14:22:52 +0700

    menambahkan data class DetailUiState

--- app/src/main/java/com/example/pertemuan10/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/DetailMhsViewModel.kt
@@ -63,7 +63,17 @@ class DetailMhsViewModel (
 /*
 Data class untuk menampung data yang akan ditampilkan diUI
 * */
-
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent ()
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
 // memindahkan data dari entity ke ui
 fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent{
     return MahasiswaEvent(
