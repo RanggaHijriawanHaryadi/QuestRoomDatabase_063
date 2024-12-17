e6e9b3a489f22753c08c040854b856bbf1283d3b
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-17 13:40:48 +0700

    menambahkan HomeUiState dengan  getAllMhs

--- app/src/main/java/com/example/pertemuan10/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/HomeMhsViewModel.kt
@@ -4,11 +4,25 @@ package com.example.pertemuan10.ui.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.pertemuan10.data.entity.Mahasiswa
 import com.example.pertemuan10.repository.RepositoryMhs
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
 
 class HomeMhsViewModel(
     private val repositoryMhs: RepositoryMhs
 ): ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .
+
 
 }
 
